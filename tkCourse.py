07ff57033dcd66bb8dd2e24bfd3103ea48f0b932
Phil Martel <pmartel@alum.mit.edu>
2014-09-27 19:47:37 -0400

    indented properly

--- tkCourse.py
+++ tkCourse.py
@@ -3,18 +3,18 @@
 # from http://sebsauvage.net/python/gui/#our_project
 # this was written for Python 2.7
 
- import Tkinter
+import Tkinter
 
- class simpleapp_tk(Tkinter.Tk):
-     def __init__(self,parent):
-         Tkinter.Tk.__init__(self,parent)
-         self.parent = parent
-         self.initialize()
+class simpleapp_tk(Tkinter.Tk):
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
 
-     def initialize(self):
-         pass 
+    def initialize(self):
+        pass 
 
- if __name__ == "__main__":
-     app = simpleapp_tk(None)
-     app.title('my application')
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('my application')
     app.mainloop()
