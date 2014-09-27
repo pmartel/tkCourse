780637ea4c50fa35889d13594c2f823ca85e4899
Phil Martel <pmartel@alum.mit.edu>
2014-09-27 19:45:46 -0400

    Initial entries

---
+++ tkCourse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+# from http://sebsauvage.net/python/gui/#our_project
+# this was written for Python 2.7
+
+ import Tkinter
+
+ class simpleapp_tk(Tkinter.Tk):
+     def __init__(self,parent):
+         Tkinter.Tk.__init__(self,parent)
+         self.parent = parent
+         self.initialize()
+
+     def initialize(self):
+         pass 
+
+ if __name__ == "__main__":
+     app = simpleapp_tk(None)
+     app.title('my application')
+    app.mainloop()
